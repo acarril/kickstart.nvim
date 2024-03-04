b62eadea131e6b897253beb3efa43973e449729e
acarril <acarril@princeton.edu>
2024-03-04 16:21:17 -0500

    explicitly set termguicolors

--- init.lua
+++ init.lua
@@ -148,6 +148,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Term GUI colors
+vim.opt.termguicolors = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
