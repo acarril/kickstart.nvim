b4c691f4a3d7ea7b8bcfbc5bb05e241f2948a63e
acarril <acarril@princeton.edu>
2024-03-04 16:12:23 -0500

    make LSP notifications plugin look nicer with colorscheme + transparent bg

--- init.lua
+++ init.lua
@@ -408,7 +408,14 @@ require('lazy').setup {
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      {
+        'j-hui/fidget.nvim',
+        opts = {
+          notification = {
+            window = { normal_hl = 'Info', winblend = 0, border = 'single' },
+          },
+        },
+      },
     },
     config = function()
       -- Brief Aside: **What is LSP?**
