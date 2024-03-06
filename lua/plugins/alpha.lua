8371d2666a7e461104a96b8505dc3f3876b9a0d7
acarril <acarril@princeton.edu>
2024-03-06 15:05:46 -0500

    add greeter

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,9 @@
+return {
+  { -- Greeter screen
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').config)
+    end,
+  },
+}
