145a750758cfda03d9bda8e20c6dbb8f34cc0d85
acarril <acarril@princeton.edu>
2024-03-07 10:36:12 -0500

    hl current scope indent

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -17,8 +17,10 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      -- Indent current scope
+      -- NOTE: works in tandem with indent-blankline.nvim,
+      -- which provides static indent guides
+      require('mini.indentscope').setup {}
     end,
   },
 }
