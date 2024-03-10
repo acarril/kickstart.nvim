fd6fa8ec8c27c870196ebd4426a836341563f809
acarril <acarril@princeton.edu>
2024-03-10 12:20:48 -0400

    workaround for EdenEast/nightfox.nvim#421: solid bg in lualine_c

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,14 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require('lualine').setup()
+      -- Create a custom 'nighfox' theme with solid bg in `lualine_c`
+      -- NOTE: workaround for https://github.com/EdenEast/nightfox.nvim/issues/421
+      local palette = require('nightfox.palette').load 'nightfox'
+      local custom_nightfox = require 'lualine.themes.nightfox'
+      custom_nightfox.normal.c.bg = palette.bg0
+
+      -- Load lualine
+      require('lualine').setup { options = { theme = custom_nightfox } }
     end,
   },
 }
