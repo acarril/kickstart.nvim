ba1849ecdc543d55007617d9f2df9cffd295ec2f
acarril <acarril@princeton.edu>
2024-03-13 10:33:16 -0400

    tweak hl groups to handle transparent bg better

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -8,23 +8,31 @@ return {
     local style = 'nightfox'
 
     -- Returns the palette of the specified colorscheme
+    -- NOTE: see https://github.com/EdenEast/nightfox.nvim/blob/main/usage.md#palette
     local palette = require('nightfox.palette').load(style)
 
     -- Configure colorscheme with transparent background plus some tweaks
     -- NOTE: this should work with opts = {}? I think `config` runs before `opts`
     -- so the colorscheme is set before passing opts.
     require('nightfox').setup {
+      -- bg transparency for seamless integration with terminal colors
       options = { transparent = true },
+
+      -- however, if terminal opacity < 1, some highlights are not visible
       groups = {
         all = {
-          TelescopeBorder = { fg = palette.cyan.dim },
+          -- Telescope borders are too dim to be readable
+          TelescopeBorder = { fg = palette.fg3 },
+        },
+        nightfox = {
+          -- Change Visual highlight to differentiate from cursorline highline
+          Visual = { bg = palette.bg1 }, -- default: `bg1`; good: `sel1`, `bg0`
         },
       },
     }
 
     -- Load the colorscheme here
     vim.cmd.colorscheme(style)
-    vim.api.nvim_set_hl(0, 'lualine_c_normal', { bg = 'pink', fg = '#abb2bf' })
 
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=italic'
